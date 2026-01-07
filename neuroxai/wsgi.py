32f36121c14c5c9a03bdfa34fc1ae27466b6a191
ShoxruxShokirov <https://github.com/ShoxruxShokirov/ClothesSite>
2026-01-07 16:41:50 +0500

    Refactor code structure and improve readability

---
+++ neuroxai/wsgi.py
@@ -0,0 +1,21 @@
+"""
+WSGI config for neuroxai project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+# Use production settings if in production environment
+if os.getenv('DJANGO_ENV') == 'production':
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuroxai.settings_production')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'neuroxai.settings')
+
+application = get_wsgi_application()
+
